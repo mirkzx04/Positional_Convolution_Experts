0ec4b09cb1a0922f37a6f312d9581117c739f44b
Mirko Bonifazi <mirkzx@mac-di-Mirkzx.local>
2025-06-08 15:42:34 +0200

    Update Cifar10 class

--- Datasets_Classes/Cifar10.py
+++ Datasets_Classes/Cifar10.py
@@ -5,107 +5,135 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-class CIFAR10Dataset(Dataset):
-    def __init__(self, path):
+class CIFAR10TrainDataset(Dataset):
+    def __init__(self, cifar10_dataset):
         """
-        Constructor of CIFAR10Datase
+        Constructor of CIFAR10TrainDataset
 
         Args :
-            Path -> dataset path
+            cifar10_dataset -> CIFAR10Dataset object
 
         path -> Dataset path
         data -> Images data
         lables -> Images labels
         """
         super().__init__()
+        self.path = cifar10_dataset.path
+
+        self.data = cifar10_dataset.data_train
+        self.lables = cifar10_dataset.labels_train
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        """
+        Get item from dataset by index
+
+        Args:
+            idx -> Index of the item
+
+        Returns:
+            Tuple of image and label
+        """
+        imgaes = self.data[idx]
+        labels = self.lables[idx]
+
+        return imgaes, labels
+
+class CIFAR10ValidationDataset(Dataset):
+    def __init__(self, cifar10_dataset):
+        """
+        Constructor of CIFAR10ValidationDataset
+
+        Args :
+            cifar10_dataset -> CIFAR10Dataset object
+        """
+        super().__init__()
+        self.path = cifar10_dataset.path
+
+        self.data = cifar10_dataset.data_validation
+        self.lables = cifar10_dataset.labels_validation
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        """
+        Get item from dataset by index
+
+        Args:
+            idx -> Index of the item
+
+        Returns:
+            Tuple of image and label
+        """
+        imgaes = self.data[idx]
+        labels = self.lables[idx]
+
+        return imgaes, labels
+
+class CIFAR10Dataset(Dataset):
+    def __init__(self, path):
+        """
+        Constructor of CIFAR10 dataset
+
+        Args :
+            Path -> dataset path
+        """
+        super().__init__()
         self.path = path
-        self.data = []
-        self.lables = []
-        
+
+        self.data_train = []
+        self.labels_train = []
+
+        self.data_validation = []
+        self.labels_validation = []
+
         # Load batch data from path
+        self.load_batch_data()
+        self.load_batch_val()
+
+    def load_batch_data(self):
         for idx in range(1,6):
             batch_path = f'{self.path}{str(idx)}'
-            batch_data = self.load_batch(path = batch_path)
+            batch_data = self.read_batch_path(path = batch_path)
             
             # Reshape IMG from [10000, 3072] to [10000, 3, 32,32]
             imgs_data = batch_data[b'data']
             imgs_data = imgs_data.reshape(10000, 3, 32,32)
 
-            # Upscale Image from 32x32 to 128x128
-            upscale_img = self.upscale_img(imgs_data)
-            self.data.extend(upscale_img)
+            self.data_train.extend(imgs_data)
+            self.labels_train.extend(batch_data[b'labels'])
 
-            self.lables.extend(batch_data[b'labels'])
+        self.data_train = np.array(self.data)
+        self.labels_train = np.array(self.lables)
 
-        self.data = np.array(self.data)
-        self.lables = np.array(self.lables)
+    def load_batch_val(self):
+        """
+        Load validation batch data
+        """
+        batch_path = f'./Data/cifar-10-batches-py/test_batch'
+        batch_data = self.read_batch_path(path = batch_path)
+
+        # Reshape IMG from [10000, 3072] to [10000, 3, 32,32]
+        imgs_data = batch_data[b'data']
+        imgs_data = imgs_data.reshape(10000, 3, 32,32)
+        
+        self.data_validation.extend(imgs_data)
+        self.labels_validation.extend(batch_data[b'labels'])
 
     # Unpickle function for cifar10 data
-    def load_batch(self, path):
+    def read_batch_path(self, path):
         with open(path, 'rb') as fo:
             dict = pickle.load(fo, encoding='bytes')
         return dict
     
     def __len__(self):
-        return len(self.data)
-    
+        return len(self.data_train)
+
     def __getitem__(self, idx):
-        imgaes = self.data[idx]
-        labels = self.lables[idx]
+        imgaes = self.data_train[idx]
+        labels = self.labels_train[idx]
 
         return imgaes, labels
-    
-    def upscale_img(self, img, target_size = (128,128)):
-        """
-        Upscale dataset images
-        """
-        upscaled_img = []
-
-        # Upscale image into batch
-        for i, img in enumerate(img):
-            # Convert shape from [3,32,32] to [32,32,3]
-            img_hwc = img.transpose(1,2,0)
-
-            upscaled = cv2.resize(
-                img_hwc,
-                target_size,
-                interpolation=cv2.INTER_CUBIC
-            )
-            
-            # Convert shape from [128,128,3] to [3,128,128]
-            upscaled_img.append(upscaled.transpose(2,0,1))
-
-        return np.array(upscaled_img)
-
-    def show_images(self, num_img, idx):
-        """
-        Function for visualize some images
-        Args:
-            num_img -> Number of the images that want visualize
-            idx -> Index of the images that want visualize, must be an array even of len 1
-        """
-        
-        fig, axes = plt.subplots(2,4,figsize = (10,5))
-        if idx is None:
-            for i in range(num_img):
-                img, lable = self.data[i], self.lables[i]
-                print(img.shape)
-                ax = axes[i//4, i %4]
-
-                # Change channel from [C,H,W] to [H,W,C]
-                ax.imshow(img.transpose(1,2,0))
-                ax.set_title(f'Classes : {lable}')
-                ax.axis('off')
-            
-            plt.show()
-        else:
-            for index in idx:
-                img, lable = self.data[i], self.lables[i]
-                ax = axes[i//4, i %4]
-
-                # Change channel from [C,H,W] to [H,W,C]
-                ax.imshow(img.permute(1,2,0))
-                ax.set_title(f'Classes : {lable}')
-                ax.axis('off')
-            
-            plt.show()
